235efcaea1e9f85c4aaa3fbe63b9bed2b078339a
Shohidul Arifin <shohidularifin@gmail.com>
2023-04-28 04:29:47 -0700

    Sign Up Loginand logout authentication completed

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,48 @@
-import React from 'react';
-
+import React, { useContext } from 'react';
+import './Login.css'
+import { AuthContext } from '../providers/AuthProvider';
+import { Link } from 'react-router-dom';
 const Login = () => {
+    const {logIn}= useContext(AuthContext)
+
+    const handleLogin= (event)=>{
+        event.preventDefault();
+        const form= event.target;
+        const email =form.email.value;
+        const password= form.password.value;
+        console.log(email,password);
+        logIn(email,password)
+        .then(result=>{
+            const loggedUser = result.user;
+            console.log(loggedUser);
+            form.reset();
+
+        })
+        .catch(error=>{
+            console.log(error);
+        })
+
+    }
     return (
-        <div>
-            <h2>Login coming sooon</h2>
+        <div className='form-container'>
+            <h2 className='form-title'>Login</h2>
+            <form onSubmit={handleLogin}>
+                <div className="form-control">
+                <label htmlFor="">Email</label>
+                <input type="email" name="email" id="" required/>
+                
+                </div>
+                <div className="form-control">
+                
+                <label htmlFor="">Password</label>
+                <input type="password" name="password" id="" required/>
+                
+                </div>
+                <input className='btn-submit' type="submit" value="Login" />
+
+                
+            </form>
+            <p><small>New to Ema-john? <Link to='/signup'>Create new account</Link></small></p>
         </div>
     );
 };
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB26YjbrCJybmVyJCOXS9RXD5vm8moxAdM",
+  authDomain: "ema-john-with-firebase-a-dfb05.firebaseapp.com",
+  projectId: "ema-john-with-firebase-a-dfb05",
+  storageBucket: "ema-john-with-firebase-a-dfb05.appspot.com",
+  messagingSenderId: "835986978672",
+  appId: "1:835986978672:web:d04f574c9f151962e381b5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
